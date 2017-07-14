fcb363897d16831f1377e9533f3aae093d43debc
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-07-14 22:14:44 +1200

    Update styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -45,7 +45,7 @@ $userName = 'Steve Clifton';
                 <!-- Main content -->
                 <section class="content">
                     <div class="row">
-                        <section class="col-lg-8 pull-left" style="padding-left: 20px">
+                        <section class="col-xs-8 col-sm-8 col-lg-8 pull-left">
                             <div class="box box-info">
                                 <div class="box-header with-border">
                                     <h3 class="box-title">Upcoming Events</h3>
@@ -123,7 +123,7 @@ $userName = 'Steve Clifton';
                                 <!-- /.box-footer -->
                             </div>
                         </section>
-                        <section class="col-lg-4 pull-right">
+                        <section class="col-xs-4 col-sm-4 col-lg-4 pull-right">
                             <div class="box box-primary">
                                 <div class="box-header with-border">
                                     <h3 class="box-title">Previous Results</h3>
