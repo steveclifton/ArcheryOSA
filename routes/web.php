4a40f3ef7eb014a48b5f104f520ed1ad41db4722
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-07-18 22:21:43 +1200

    Remove unused text

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,8 @@
 <?php
 
-use App\Club;
-use App\Event;
-use App\Round;
-use App\Roundgroup;
-use App\Roundtype;
-use App\User;
-use App\District;
-
 Route::get('/', 'Home@index')->name('home');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
-
-
-
-
-// Authentication routes...
-// Route::get('auth/login', 'Auth\AuthController@getLogin');
-// Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('logout', 'auth\LogoutController@logout');
-
-// Registration routes...
-//Route::get('/register', 'Auth\RegisterController@getRegister');
-// Route::post('auth/register', 'Auth\AuthController@postRegister');
-
-
-// Route::controllers([
-//    'password' => 'Auth\PasswordController',
-// ]);
