78e03cfd7724b222d8c5eaa0391997acdc6c5e9f
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-10-08 19:06:37 +1300

    Add events

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Event;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
 
     public function index()
     {
-        return view ('includes.welcome');
+        $events = DB::select("SELECT *
+                        FROM `events`
+                        WHERE `deleted` = 0
+                        AND `status` IN ('open', 'waitlist', 'pending')
+                        AND `visible` = 1
+                        ORDER BY `startdate` DESC
+                        ");
+
+
+        return view ('includes.welcome', compact('events'));
     }
 }
