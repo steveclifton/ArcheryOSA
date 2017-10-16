ddb35d2c94ac26cc350c88c088001f82e6db4efb
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-10-16 20:22:05 +1300

    Comment out unused code

--- resources/views/layouts/tasks.blade.php
+++ resources/views/layouts/tasks.blade.php
@@ -1,30 +1,30 @@
 <!-- Tasks: style can be found in dropdown.less -->
-<li class="dropdown tasks-menu">
-    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-        <i class="fa fa-flag-o"></i>
-        <span class="label label-danger">2</span>
-    </a>
-    <ul class="dropdown-menu">
-        <li class="header">You have 1 upcoming events</li>
-        <li>
-            <!-- inner menu: contains the actual data -->
-            <ul class="menu">
-                <li><!-- Task item -->
-                    <a href="#">
-                        <h3>
-                        Some task I need to do
-                        <small class="pull-right">60%</small>
-                        </h3>
-                        <div class="progress xs">
-                            <div class="progress-bar progress-bar-red" style="width: 60%" role="progressbar"
-                                aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
-                                <span class="sr-only">60% Complete</span>
-                            </div>
-                        </div>
-                    </a>
-                </li>
-                <!-- end task item -->
-            </ul>
-        </li>
-    </ul>
-</li>
+{{--<li class="dropdown tasks-menu">--}}
+    {{--<a href="#" class="dropdown-toggle" data-toggle="dropdown">--}}
+        {{--<i class="fa fa-flag-o"></i>--}}
+        {{--<span class="label label-danger">2</span>--}}
+    {{--</a>--}}
+    {{--<ul class="dropdown-menu">--}}
+        {{--<li class="header">You have 1 upcoming events</li>--}}
+        {{--<li>--}}
+            {{--<!-- inner menu: contains the actual data -->--}}
+            {{--<ul class="menu">--}}
+                {{--<li><!-- Task item -->--}}
+                    {{--<a href="#">--}}
+                        {{--<h3>--}}
+                        {{--Some task I need to do--}}
+                        {{--<small class="pull-right">60%</small>--}}
+                        {{--</h3>--}}
+                        {{--<div class="progress xs">--}}
+                            {{--<div class="progress-bar progress-bar-red" style="width: 60%" role="progressbar"--}}
+                                {{--aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">--}}
+                                {{--<span class="sr-only">60% Complete</span>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                    {{--</a>--}}
+                {{--</li>--}}
+                {{--<!-- end task item -->--}}
+            {{--</ul>--}}
+        {{--</li>--}}
+    {{--</ul>--}}
+{{--</li>--}}
