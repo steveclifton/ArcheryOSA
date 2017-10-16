5971314384a544a52c9816fbe19dbc4252b6e29d
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-10-16 20:22:49 +1300

    Get users entered events

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Event;
+use App\EventEntry;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
@@ -19,7 +21,13 @@ class HomeController extends Controller
                         ORDER BY `startdate` DESC
                         ");
 
+        $userevents = DB::select("SELECT e.`eventid`, e.`startdate`, es.`name` as usereventstatus, e.`name`, e.`status` as eventstatus 
+                        FROM `evententry` ee
+                        LEFT JOIN `events` e USING (`eventid`)
+                        LEFT JOIN `entrystatuses` es ON (ee.`entrystatusid` = es.`entrystatusid`)
+                        WHERE ee.`userid` = '" . Auth::id(). "'
+                        ");
 
-        return view ('includes.welcome', compact('events'));
+        return view ('includes.welcome', compact('events', 'userevents'));
     }
 }
