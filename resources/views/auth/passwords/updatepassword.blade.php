633411d2bab9c659c0c4c7d4ae72148506bbf6a0
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-10-17 21:49:17 +1300

    Remove

---
+++ resources/views/auth/passwords/updatepassword.blade.php
@@ -0,0 +1,59 @@
+@include('layouts.title', ['title'=>'Update Password'])
+
+@extends('home')
+
+@section('content')
+
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Update Password</div>
+                    <div class="panel-body">
+
+
+                        <form class="form-horizontal" method="POST" action="{{ route('updatepassword') }}">
+                            {{ csrf_field() }}
+
+                            <input type="hidden" name="hash" value="{{$hash}}">
+
+                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                <label for="password" class="col-md-4 control-label">Password</label>
+
+                                <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control" name="password" required>
+
+                                    @if ($errors->has('password'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+
+                                <div class="col-md-6">
+                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                </div>
+                            </div>
+
+
+                            <div class="form-group">
+                                <div class="col-md-8 col-md-offset-4">
+                                    <button type="submit" class="btn btn-warning">
+                                        Reset Password
+                                    </button>
+
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
