900f7d45bb9ce350d7c0a1d3512040930f2f3a31
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-10-17 22:43:52 +1300

    Removed field

--- resources/views/layouts/leftsidebar.blade.php
+++ resources/views/layouts/leftsidebar.blade.php
@@ -131,18 +131,18 @@
                 {{--</ul>--}}
             {{--</li>--}}
 
-            <li class="treeview">
-                <a href="#">
-                    <i class="fa fa-users"></i> <span>Neighbourhood</span>
-                    <span class="pull-right-container">
-                        <i class="fa fa-angle-left pull-right"></i>
-                    </span>
-                </a>
-                <ul class="treeview-menu">
-                    <li class="margin15"><a href="{{route('public.clubs')}}"><i class="fa fa-user-o"></i>Clubs</a></li>
+            {{--<li class="treeview">--}}
+                {{--<a href="#">--}}
+                    {{--<i class="fa fa-users"></i> <span>Neighbourhood</span>--}}
+                    {{--<span class="pull-right-container">--}}
+                        {{--<i class="fa fa-angle-left pull-right"></i>--}}
+                    {{--</span>--}}
+                {{--</a>--}}
+                {{--<ul class="treeview-menu">--}}
+                    {{--<li class="margin15"><a href="{{route('public.clubs')}}"><i class="fa fa-user-o"></i>Clubs</a></li>--}}
                     {{--<li class="margin15"><a href="#"><i class="fa fa-user-o"></i>National Bodies</a></li>--}}
-                </ul>
-            </li>
+                {{--</ul>--}}
+            {{--</li>--}}
 
             {{--<li class="treeview">--}}
                 {{--<a href="#">--}}
