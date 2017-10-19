be521d4e68c5e71b4c9baa400f9cf3acc895ad4d
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-10-19 20:24:56 +1300

    Add recaptcha rule

---
+++ app/Rules/Recaptcha.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Rules;
+
+use GuzzleHttp\Client;
+use Illuminate\Contracts\Validation\Rule;
+
+class Recaptcha implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+
+        $client = new Client();
+
+        $response = $client->post(
+            'https://www.google.com/recaptcha/api/siteverify',
+            ['form_params'=>
+                [
+                    'secret'=>env('GOOGLE_RECAPTCHA_SECRET'),
+                    'response'=>$value
+                ]
+            ]
+        );
+
+        $body = json_decode((string)$response->getBody());
+        return $body->success;
+
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'Please ensure you are not a bot!';
+    }
+}
