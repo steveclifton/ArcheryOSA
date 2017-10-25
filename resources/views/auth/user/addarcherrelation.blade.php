0d7ff0c25a8dc8634ba329fd1ef872be4e9be05b
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-10-26 08:35:12 +1300

    Add form for adding relation

---
+++ resources/views/auth/user/addarcherrelation.blade.php
@@ -0,0 +1,72 @@
+@section ('dashboard')
+    <h1></h1>
+@endsection
+
+@include('layouts.title', ['title'=>'Add Archer Relation'])
+
+@extends ('home')
+
+@section ('content')
+
+
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            @if(session()->has('message'))
+                <div class="alert alert-success">
+                    {{ session()->get('message') }}
+                </div>
+            @elseif (session()->has('failure'))
+                <div class="alert alert-danger">
+                    {{ session()->get('failure') }}
+                </div>
+            @endif
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Add Archer Relationship
+                    <a href="{{route('profile')}}">
+                        <button type="submit" class="btn btn-default pull-right" >
+                            <i class="fa fa-backward" > Back</i>
+                        </button>
+                    </a>
+                </div>
+
+
+                <div class="panel-body">
+                    <form class="form-horizontal" method="POST" action="{{ route('createarcherrelationship') }}" >
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">Archer's Email Address</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{old('email')}}" required autofocus>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-success" id="savebutton" value="save" name="submit">
+                                    Request Relation
+                                </button>
+                            </div>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
