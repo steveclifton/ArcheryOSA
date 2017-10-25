1ee67fed1b01f323ddd8c8b4166e2225dada610d
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-10-26 08:30:43 +1300

    Close entry box on page load

--- resources/views/auth/events/updateevent.blade.php
+++ resources/views/auth/events/updateevent.blade.php
@@ -27,11 +27,11 @@
             <div class="row">
                 <div class="col-md-12">
                     <div>
-                        <div class="box box-info">
+                        <div class="box box-info collapsed-box">
                             <div class="box-header with-border">
-                                <h3 class="box-title">Event Entries</h3>
+                                <h3 class="box-title" >Event Entries</h3>
                                 <div class="box-tools pull-right">
-                                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                                    <button type="button" class="btn btn-box-tool" data-widget="collapse">Click to Open <i class="fa fa-minus"></i>
                                     </button>
                                 </div>
                             </div>
@@ -70,6 +70,7 @@
                                                             $colour = '#FAAEAE';
                                                         break;
                                                     }
+
                                                 @endphp
 
                                                 <tr onmouseover="this.style.backgroundColor='lightgrey'" onmouseout="this.style.backgroundColor='{{$colour}}'" style="background: {{$colour}}">
