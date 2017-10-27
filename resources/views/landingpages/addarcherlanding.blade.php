c4576af07b232d0b70e996e96351ca14a985707b
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-10-27 21:55:36 +1300

    Add landing page for acceptance of relation

---
+++ resources/views/landingpages/addarcherlanding.blade.php
@@ -0,0 +1,37 @@
+@section ('dashboard')
+    <h1></h1>
+@endsection
+
+@extends ('home')
+
+@section ('title')Confirm Archer Relation @endsection
+
+@section ('content')
+
+
+
+
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-md-10 col-md-offset-1">
+                            @if(!empty($success))
+                                <div class="alert alert-success" style="text-align: center">
+                                    {{ $success }}
+                                </div>
+                            @elseif(!empty($failure))
+                                <div class="alert alert-danger" style="text-align: center">
+                                    {{ $failure }}
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
