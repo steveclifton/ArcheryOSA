cc9898f9a92e35ff99708b2fe7fbcc9c75b5b0d9
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-11-03 21:56:33 +1300

    Update domain

--- config/session.php
+++ config/session.php
@@ -148,7 +148,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', null),
+    'domain' => 'archeryosa.com',//env('SESSION_DOMAIN', null),
 
     /*
     |--------------------------------------------------------------------------
