845ef630d9c7d7b66f26aa2f3acdc27866f8879f
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-11-04 19:32:06 +1300

    Add formating

--- resources/views/auth/events/updateevent.blade.php
+++ resources/views/auth/events/updateevent.blade.php
@@ -29,9 +29,10 @@
                     <div>
                         <div class="box box-info collapsed-box">
                             <div class="box-header with-border">
-                                <h3 class="box-title" >Event Entries</h3>
+                                <h3 class="box-title">Event Entries</h3>
                                 <div class="box-tools pull-right">
-                                    <button type="button" class="btn btn-box-tool" data-widget="collapse">Click to Open <i class="fa fa-minus"></i>
+                                    <button type="button" class="btn btn-box-tool" data-widget="collapse">Click to Open &nbsp;
+                                        <i class="fa fa-minus"></i>
                                     </button>
                                 </div>
                             </div>
