23bf550fffb82d9f72a9143916269a0e1097f37b
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-11-04 19:56:31 +1300

    Add include

---
+++ resources/views/includes/session_errors.blade.php
@@ -0,0 +1,9 @@
+@if(session()->has('message'))
+    <div class="alert alert-success">
+        {{ session()->get('message') }}
+    </div>
+@elseif (session()->has('failure'))
+    <div class="alert alert-danger">
+        {{ session()->get('failure') }}
+    </div>
+@endif
