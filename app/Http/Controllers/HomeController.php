49ccb88ae4a9f68fc19cc7eb550a1c0225d621f9
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-11-05 17:17:54 +1300

    Add user events

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,8 +21,13 @@ class HomeController extends Controller
                         ORDER BY `startdate` DESC
                         ");
 
+        $userevents = DB::select("SELECT e.`eventid`, e.`startdate`, es.`name` as usereventstatus, e.`name`, e.`status` as eventstatus 
+                        FROM `evententry` ee
+                        LEFT JOIN `events` e USING (`eventid`)
+                        LEFT JOIN `entrystatuses` es ON (ee.`entrystatusid` = es.`entrystatusid`)
+                        WHERE ee.`userid` = '" . Auth::id(). "'
+                        ");
 
-
-        return view ('includes.welcome', compact('events'));
+        return view ('includes.welcome', compact('events', 'userevents'));
     }
 }
