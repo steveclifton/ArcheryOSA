77122bcf9375488512cc176cb4679e61bebc643f
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-11-05 17:17:31 +1300

    Add image to table row

--- resources/views/admin/users/users.blade.php
+++ resources/views/admin/users/users.blade.php
@@ -17,6 +17,7 @@
                     <table class="table table-hover">
                         <thead>
                             <tr>
+                                <th>Image</th>
                                 <th>User ID</th>
                                 <th>User Type</th>
                                 <th>First Name</th>
@@ -29,14 +30,17 @@
                             @foreach ($users as $user)
                                 <tr>
                                     <td>
+                                        <img src="/content/profile/{{$user->image}}" alt="" height="42" width="42">
+                                    </td>
+                                    <td>
                                         <a href="{{route('getuserprofile', $user->userid)}}">
                                             {{$user->userid}}
                                         </a>
                                     </td>
-                                    <td>{{ucwords($user->usertype)}}</td>
-                                    <td>{{ucwords($user->firstname)}}</td>
-                                    <td>{{ucwords($user->lastname)}}</td>
-                                    <td>{{ucwords($user->email)}}</td>
+                                    <td>{{ucwords(trim($user->usertype))}}</td>
+                                    <td>{{ucwords(trim($user->firstname))}}</td>
+                                    <td>{{ucwords(trim($user->lastname))}}</td>
+                                    <td>{{ucwords(trim($user->email))}}</td>
                                     <td>{{$user->eventcount}}</td>
 
                                 </tr>
