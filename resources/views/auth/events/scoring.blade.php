155467deee664b58d273978ac03c94927ef232bd
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-11-07 22:38:29 +1300

    Add scoring view

--- resources/views/auth/events/scoring.blade.php
+++ resources/views/auth/events/scoring.blade.php
@@ -8,75 +8,119 @@
 
 @section ('content')
 
+    {{--<div class="box-header">--}}
+        {{--<h3 class="box-title">Responsive Hover Table</h3>--}}
+
+        {{--<div class="box-tools">--}}
+            {{--<div class="input-group input-group-sm" style="width: 150px;">--}}
+                {{--<input type="text" name="table_search" class="form-control pull-right" placeholder="Search">--}}
+
+                {{--<div class="input-group-btn">--}}
+                    {{--<button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+
     {{-- <div class="container"> --}}
     <div class="row">
         <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">Create New Event
-                    <a href="{{route('events')}}">
-                        <button type="submit" class="btn btn-default pull-right" id="addevent">
-                            <i class="fa fa-backward" > Back</i>
-                        </button>
-                    </a>
+            <div class="box">
+                <div class="box-header">
+                    <h3 class="box-title"><strong>Scoring</strong></h3><BR>
+                    <h3 class="box-title">{{ ucwords($event->name)}}</h3>
+
+                    <div class="box-tools">
+                        <div class="input-group input-group-sm" style="width: 150px;">
+                            <a href="{{URL::previous()}}">
+                                <button type="submit" class="btn btn-primary pull-right" id="addevent">
+                                    <i class="fa fa-backward" >
+                                        <span style="font-family: sans-serif;"> Back</span>
+                                    </i>
+                                </button>
+                            </a>
+                        </div>
+                    </div>
+                    <hr>
                 </div>
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('createevent') }}" id="eventform">
+
+            <div class="box-body">
+                {{--<div class="panel-heading">{!! ucwords($event->name) . ' - Scoring' !!}--}}
+                    {{--<a href="{{route('events')}}">--}}
+                        {{--<button type="submit" class="btn btn-default pull-right" id="addevent">--}}
+                            {{--<i class="fa fa-backward" > Back</i>--}}
+                        {{--</button>--}}
+                    {{--</a>--}}
+                {{--</div>--}}
+
+
+                {{--<div class="panel-body">--}}
+                    <form class="form-horizontal" method="POST" action="{{ route('enterscores', [$eventround[0]->eventroundid, $event->eventid])  }}" id="eventform">
                         {{ csrf_field() }}
 
-                        <div class="form-group {{ $errors->has('scoredate') ? ' has-error' : '' }}">
-                            <div class="col-md-6">
-                                <label class="col-md-3 control-label">Scoring Date</label>
-                                    <div class="input-group date">
-                                        <div class="input-group-addon">
-                                            <i class="fa fa-calendar"></i>
-                                        </div>
-                                        <input type="text" name="scoredate" class="form-control pull-right" id="scoredate" >
-                                    </div>
-                                @if ($errors->has('scoredate'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('scoredate') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        <div class=" table-condensed table-striped table-responsive">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th class="hidden">UserID</th>
+                                        <th class="col-md-1 col-xs-1 col-sm-1">Archer</th>
+                                        <th class="col-md-1 col-xs-1 col-sm-1">Division</th>
+
+                                        @if (isset($distances['Distance-1']))
+                                            <th class="col-md-1 col-xs-1 col-sm-1" style="text-align: center;">{!! $distances['Distance-1'] . $eventround[0]->unit !!}</th>
+                                        @endif
+                                        @if (isset($distances['Distance-2']))
+                                            <th class="col-md-1 col-xs-1 col-sm-1" style="text-align: center;">{!! $distances['Distance-2'] . $eventround[0]->unit !!}</th>
+                                        @endif
+                                        @if (isset($distances['Distance-3']))
+                                            <th class="col-md-1 col-xs-1 col-sm-1" style="text-align: center;">{!! $distances['Distance-3'] . $eventround[0]->unit !!}</th>
+                                        @endif
+                                        @if (isset($distances['Distance-4']))
+                                            <th class="col-md-1 col-xs-1 col-sm-1" style="text-align: center;">{!! $distances['Distance-4'] . $eventround[0]->unit !!}</th>
+                                        @endif
+                                        <th class="col-md-1 col-xs-1 col-sm-1" style="text-align: center;">Total</th>
+                                        <th class="col-md-1 col-xs-1 col-sm-1" style="text-align: center;">Hits</th>
+                                        <th class="col-md-1 col-xs-1 col-sm-1" style="text-align: center;">10s</th>
+                                        <th class="col-md-1 col-xs-1 col-sm-1" style="text-align: center;">X</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($users as $user)
+
+                                    {{--NEED LOGIC FOR VALIDATION --}}
+
+                                    <tr>
+                                        <td class="hidden">
+                                            <input type="hidden" name="userid[]" value="{{$user->userid}}"></td>
+                                        <td>{{$user->fullname}}</td>
+                                        <td>{{$user->divisionname}}</td>
+
+                                        @if(isset($distances['Distance-1'])) <td>
+                                            <input type="text" class="form-control" name="dist1[]">
+                                        </td> @endif
+                                        @if(isset($distances['Distance-2'])) <td>
+                                            <input type="text" class="form-control" name="dist2[]">
+                                        </td> @endif
+                                        @if(isset($distances['Distance-3'])) <td>
+                                            <input type="text" class="form-control" name="dist3[]">
+                                        </td> @endif
+                                        @if(isset($distances['Distance-4'])) <td>
+                                            <input type="text" class="form-control" name="dist4[]">
+                                        </td> @endif
+
+                                        <td><input type="text" class="form-control" name="total[]"></td>
+                                        <td><input type="text" class="form-control" name="hit[]" placeholder="Optional"></td>
+                                        <td><input type="text" class="form-control" name="10[]" placeholder="Optional"></td>
+                                        <td><input type="text" class="form-control" name="x[]" placeholder="Optional"></td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
                         </div>
-                        <hr>
-
-                        <table class="table table-condensed table-striped">
-                            <thead>
-                                <tr>
-                                    <th class="col-md-1">Fullname</th>
-                                    <th class="col-md-1">Division</th>
-                                    <th class="col-md-1">Dist 1</th>
-                                    <th class="col-md-1">Dist 2</th>
-                                    <th class="col-md-1">Dist 3</th>
-                                    <th class="col-md-1">Dist 4</th>
-                                    <th class="col-md-1">Total Score</th>
-                                    <th class="col-md-1">Hits</th>
-                                    <th class="col-md-1">10s</th>
-                                    <th class="col-md-1">X</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                        @foreach(range(1, 20) as $i)
-                            <tr>
-                                <td>Steve Clifton</td>
-                                <td>Compound</td>
-                                <td><input type="text" class="form-control"></td>
-                                <td><input type="text" class="form-control"></td>
-                                <td><input type="text" class="form-control"></td>
-                                <td><input type="text" class="form-control"></td>
-                                <td><input type="text" class="form-control"></td>
-                                <td><input type="text" class="form-control"></td>
-                                <td><input type="text" class="form-control"></td>
-                                <td><input type="text" class="form-control"></td>
-                            </tr>
-                        @endforeach
-                            </tbody>
-                        </table>
 
 
-                        <button type="submit" class="btn btn-success pull-right" value="update" name="update">
+                        <button type="submit" class="btn btn-success pull-right" value="submit" name="submit">
                             Submit Scores
                         </button>
 
@@ -84,6 +128,7 @@
                 </div>
             </div>
         </div>
+    </div>
     </div>
     {{-- </div> --}}
     <!-- daterangepicker -->
