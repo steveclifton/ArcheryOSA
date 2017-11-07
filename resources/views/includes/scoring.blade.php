5321183b2d7e6080a4e64bd55e1be2ab1919f36f
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-11-07 22:39:14 +1300

    Add scoring view

--- resources/views/includes/scoring.blade.php
+++ resources/views/includes/scoring.blade.php
@@ -1,4 +1,4 @@
-<div class="col-md-9">
+<div class="col-md-12">
     <div class="row">
         <div class="col-md-12">
             <div>
@@ -7,7 +7,7 @@
                         <div>
                             <h1 class="box-title">Scoring</h1>
                         </div>
-                        <div class="box-tools pull-right">
+                        <div class="box-tools pull-right" >
                             <button type="button" class="btn btn-box-tool" data-widget="collapse">Click to Open &nbsp;
                                 <i class="fa fa-minus"></i>
                             </button>
@@ -15,38 +15,31 @@
                     </div>
                     <div class="box-body">
                         <div class="table-responsive">
-                            <form class="form-horizontal" method="POST" action="" id="eventformupdate">
-                                {{ csrf_field() }}
-                                <table class="table">
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th>Scoring</th>
+                                    <th>Name</th>
+                                    <th class="hidden-xs">Location</th>
+                                    <th>Round</th>
+                                </tr>
+                                </thead>
 
-                                    <thead>
-                                    <tr>
-                                        <th>Status</th>
-                                        <th>Name</th>
-                                        <th>Location</th>
-                                        <th>Round</th>
-                                        <th>Scoring</th>
-                                    </tr>
-                                    </thead>
-
-                                    <tbody>
+                                <tbody>
                                     @foreach($eventrounds as $eventround)
                                         <tr onmouseover="this.style.backgroundColor='lightgrey'" onmouseout="this.style.backgroundColor='white'">
-                                            <td>{!! ucwords($event->status) !!}</td>
-                                            <td>{{ ucwords($eventround->roundname) }}</td>
-                                            <td>{{ ucwords($eventround->location) }}</td>
-                                            <td>{{ ucwords($eventround->name) }}</td>
                                             <td>
-                                                <a href="{{ route('enterscore', urlencode($event->name)) }}">
+                                                <a href="{{ route('getenterscoreview', [$eventround->eventroundid, $event->eventid, urlencode($event->name)]) }}">
                                                     <i class="fa fa-bullseye" aria-hidden="true"></i> Score
                                                 </a>
                                             </td>
+                                            <td>{{ ucwords($eventround->roundname) }}</td>
+                                            <td class="hidden-xs">{{ ucwords($eventround->location) }}</td>
+                                            <td>{{ ucwords($eventround->name) }}</td>
                                         </tr>
                                     @endforeach
-                                    </tbody>
-
-                                </table>
-                            </form>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
