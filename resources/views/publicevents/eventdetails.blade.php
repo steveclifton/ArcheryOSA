8018f2bcf0b2f2d20f4a896345bd433c711a6916
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-11-07 22:39:32 +1300

    Add new modules to view

--- resources/views/publicevents/eventdetails.blade.php
+++ resources/views/publicevents/eventdetails.blade.php
@@ -13,19 +13,37 @@
         <div class="row">
             @include('includes.scoring')
         </div>
+        <div class="row">
+            @include('includes.events.eventdetails_results')
+        </div>
     @endif
+        <div class="row">
+            @include('includes.events.eventdetails_currententries')
+        </div>
+
 
 
     <div class="row">
         <div class="col-xs-12">
             <div class="row">
-                <div class="col-md-9">
+                <div class="col-md-12">
                     @include('includes.session_errors')
                     <div>
                         <div class="box box-primary">
                             <div class="box-header with-border">
+
                                 <h3 class="box-title">Event Details</h3>
+                                <div class="box-tools pull-right">
+                                    <button type="button" class="btn btn-box-tool" data-widget="collapse" id="eventdetailboxtool">
+                                        Click to Close  &nbsp;
+                                        <i class="fa fa-minus"></i>
+                                    </button>
+                                </div>
+
                             </div>
+
+
+
                             <!-- /.box-header -->
                             <div class="box-body">
 
@@ -46,25 +64,19 @@
                                     <table class="table">
                                         @include('includes.events.eventdetails_table')
                                     </table>
-                                    {!! nl2br($event->schedule) !!}
+
+                                    <div>
+                                        <?= nl2br(substr($event->schedule, 0, 450)) ?>
+                                        <?php if (strlen($event->schedule) > 450) { ?>
+                                            <span style="font-weight: bold;text-decoration: underline;" class="readmore">...Read More</span>
+                                            <span class="moreschedule hidden">
+                                                    <?= nl2br(substr($event->schedule, 450)) ?>
+                                                </span>
+                                        <?php } ?>
+                                    </div>
+
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                </div>
-
-                {{--Previous Events--}}
-                <div class="col-md-3">
-                    <div class="box box-primary">
-                        <div class="box-header with-border">
-                            <h3 class="box-title">Current Entries</h3>
-                        </div>
-
-                        @include('includes.events.eventdetails_currententries')
-
-                        <!-- /.box-footer -->
-                        <div class="box-footer text-center showmore">
-                            <a href="javascript:;" class="uppercase" id="showmoreentries">View More Entries</a>
                         </div>
                     </div>
                 </div>
