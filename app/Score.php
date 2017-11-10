58ef8a50147696fc0bba63d778e0e1ae406c5a88
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-11-10 21:09:59 +1300

    Add Score Model

--- app/Score.php
+++ app/Score.php
@@ -3,8 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Eloquent;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Auth\Authenticatable as AuthenticableTrait;
 
-class Score extends Model
+class Score extends Eloquent implements Authenticatable
 {
-    //
+    use AuthenticableTrait;
+
+    protected $table = 'scores';
+    protected $primaryKey = 'scoreid';
+
+
 }
