a4ec2c32362c9a162f818030e98035f7e33310b5
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-11-10 21:10:20 +1300

    Add current week to events

---
+++ database/migrations/2017_11_10_074037_AddWeekToEvent.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddWeekToEvent extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('events', function($table) {
+            $table->integer('currentweek')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
