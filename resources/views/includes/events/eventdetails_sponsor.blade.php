60f2c90b597afb64d7d542e64c37bdeb30e62e65
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-11-11 10:48:59 +1300

    Add event sponsor tab

---
+++ resources/views/includes/events/eventdetails_sponsor.blade.php
@@ -0,0 +1,20 @@
+<div class="col-md-12">
+    <div class="row">
+        <div class="col-md-12">
+            <div>
+                <div class="box box-success">
+                    <div class="box-header with-border">
+                        <div>
+                            <h1 class="box-title">Sponsors and Prizes</h1>
+                        </div>
+                    </div>
+                    <div class="box-body" >
+                        <img class="dtimage" src="https://image.blackpepper.co.nz/citta/1000/400">
+                        <img class="mobimage" src="https://image.blackpepper.co.nz/citta/800/500">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
