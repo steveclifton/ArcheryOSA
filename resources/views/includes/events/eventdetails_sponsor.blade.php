dec1bac47d66f655ce8dce826c52cd5625cd5998
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-11-12 20:54:48 +1300

    Add images

--- resources/views/includes/events/eventdetails_sponsor.blade.php
+++ resources/views/includes/events/eventdetails_sponsor.blade.php
@@ -7,11 +7,15 @@
                         <div>
                             <h1 class="box-title">Sponsors and Prizes</h1>
                         </div>
+                        <div class="box-tools pull-right" >
+                            <button type="button" class="btn btn-box-tool" data-widget="collapse">Click to Close &nbsp;
+                                <i class="fa fa-minus"></i>
+                            </button>
+                        </div>
                     </div>
                     <div class="box-body" >
-                        <img class="dtimage" src="https://image.blackpepper.co.nz/citta/1000/400">
-                        <img class="mobimage" src="https://image.blackpepper.co.nz/citta/800/500">
-
+                        <img class="dtimage" src="{!! '/content/sponsor/' . $event->dtimage !!}">
+                        <img class="mobimage" src="{!! '/content/sponsor/' . $event->mobimage !!}">
                     </div>
                 </div>
             </div>
