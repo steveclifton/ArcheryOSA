f0af4a4e550c1fe47d518b8662e3dabd4337d5ac
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-11-13 20:49:20 +1300

    Remove duplicate event entrys

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,17 +15,17 @@ class HomeController extends Controller
     {
         $events = DB::select("SELECT *
                         FROM `events`
-                        WHERE `deleted` = 0
-                        AND `status` IN ('open', 'waitlist', 'pending')
+                        WHERE `status` IN ('open', 'waitlist', 'pending')
                         AND `visible` = 1
                         ORDER BY `startdate` DESC
                         ");
 
-        $userevents = DB::select("SELECT e.`eventid`, e.`startdate`, es.`name` as usereventstatus, e.`name`, e.`status` as eventstatus 
+        $userevents = DB::select("SELECT e.`eventid`, e.`startdate`, es.`name` as usereventstatus, e.`name`, e.`status` as eventstatus
                         FROM `evententry` ee
                         LEFT JOIN `events` e USING (`eventid`)
                         LEFT JOIN `entrystatuses` es ON (ee.`entrystatusid` = es.`entrystatusid`)
                         WHERE ee.`userid` = '" . Auth::id(). "'
+                        GROUP BY e.`eventid`
                         ");
 
         return view ('includes.welcome', compact('events', 'userevents'));
