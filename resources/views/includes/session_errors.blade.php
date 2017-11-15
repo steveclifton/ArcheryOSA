0ea09fb1bf664297062cbca41db7d5839d57907e
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-11-16 11:30:06 +1300

    Update session include

--- resources/views/includes/session_errors.blade.php
+++ resources/views/includes/session_errors.blade.php
@@ -1,9 +1,9 @@
 @if(session()->has('message'))
     <div class="alert alert-success">
-        {{ session()->get('message') }}
+        {!! session()->get('message') !!}
     </div>
 @elseif (session()->has('failure'))
     <div class="alert alert-danger">
-        {{ session()->get('failure') }}
+        {!! session()->get('failure') !!}
     </div>
 @endif
