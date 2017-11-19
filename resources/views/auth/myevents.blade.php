e5eb6170ee62ea22fc801007bf138729b163ba50
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-11-19 21:45:10 +1300

    Remove comments

--- resources/views/auth/myevents.blade.php
+++ resources/views/auth/myevents.blade.php
@@ -5,8 +5,6 @@
 @section ('content')
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-
-            {{--My Events--}}
             <div>
                 @if (Auth::check())
                     <div class="box box-info">
