eb0bda313ef004d863dbe0ee4aa6b75cc8e9cd27
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-11-23 20:49:25 +1300

    Update styling of nav

--- resources/views/includes/events/eventdetails_nav.blade.php
+++ resources/views/includes/events/eventdetails_nav.blade.php
@@ -1,13 +1,11 @@
 <div class="col-md-12">
     <div class="row">
-        <div class="col-md-12 pull-right" >
+        <div class="col-xs-12 col-md-12 pull-right" >
             <div class="box">
                 <div class="box-body">
                     @if ($event->visible || !is_null($userevententry))
                         @if (!is_null($userevententry))
-                            <a href="{{ route('updateeventregistrationview', $event->eventid) }}" class="btn btn-warning pull-right eventButtons" role="button">
-                                <i class="fa fa-gear" aria-hidden="true"></i> Update Entry
-                            </a>
+                            <a href="{{ route('updateeventregistrationview', $event->eventid) }}" class="btn btn-warning pull-right eventButtons" role="button">My Entry</a>
                         @else
                             <a href="{{ route('eventregistrationview', ['eventid' => $event->eventid, 'name' => urlencode($event->name)] ) }}" class="btn btn-primary pull-right eventButtons" role="button">
                                 <i class="fa fa-mail-forward" aria-hidden="true"></i> Enter
@@ -23,7 +21,8 @@
                         <a class="btn btn-success eventButtons pull-right" href="{{route('getscoringview', urlencode($event->name)) }}" role="button">Scoring</a>
                     @endif
 
-                        <a class="btn btn-info eventButtons pull-right" href="{{route('eventdetails', urlencode($event->name)) }}" role="button">Event Details</a>
+                    <a class="btn btn-info eventButtons pull-right" href="{{route('eventdetails', urlencode($event->name)) }}" role="button">Details</a>
+
                 </div>
             </div>
         </div>
