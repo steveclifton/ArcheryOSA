8a987e1f1a18b1c27e6fc8cb1d74a0b3d14aa75d
Steve <stephen.clifton.1@uni.massey.ac.nz>
2017-11-23 20:48:44 +1300

    Update resuts view

--- resources/views/auth/events/event_results.blade.php
+++ resources/views/auth/events/event_results.blade.php
@@ -79,6 +79,13 @@
                                                 <th class="col-md-1 col-xs-1 col-sm-1 alignCenter" >Hits</th>
                                                 <th class="col-md-1 col-xs-1 col-sm-1 alignCenter" >10s</th>
                                                 <th class="col-md-1 col-xs-1 col-sm-1 alignCenter" >X</th>
+
+                                                @if ($event->eventtype == 1)
+                                                    <th class="col-md-1 col-xs-1 col-sm-1 alignCenter" >Points</th>
+                                                    <th class="col-md-1 col-xs-1 col-sm-1 alignCenter" >Total Points</th>
+                                                @endif
+
+
                                             </tr>
                                         </thead>
 
@@ -110,6 +117,11 @@
                                                 <td class="alignCenter">{{$result->total_hits}}</td>
                                                 <td class="alignCenter">{{$result->total_10}}</td>
                                                 <td class="alignCenter">{{$result->total_x}}</td>
+
+                                                @if ($event->eventtype == 1)
+                                                    <td class="alignCenter">0</td>
+                                                    <td class="alignCenter">0</td>
+                                                @endif
                                             </tr>
                                         @endforeach
 
