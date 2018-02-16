e1f1703e57f21db04aeea03a022ce32c786a00a6
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-02-17 11:24:38 +1300

    Update scoring form

--- resources/views/auth/events/event_scoringrounds.blade.php
+++ resources/views/auth/events/event_scoringrounds.blade.php
@@ -45,21 +45,21 @@
 
                         <table class="table">
                             <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th class="hidden-xs">Location</th>
-                                    <th>Round</th>
-                                </tr>
+                            <tr>
+                                <th>Name</th>
+                                <th class="hidden-xs">Location</th>
+                                <th>Round</th>
+                            </tr>
                             </thead>
 
                             <tbody>
-                                @foreach($eventrounds as $eventround)
-                                    <tr onmouseover="this.style.backgroundColor='lightgrey'" onmouseout="this.style.backgroundColor='white'">
-                                        <td>{{ ucwords($eventround->roundname) }}</td>
-                                        <td class="hidden-xs">{{ ucwords($eventround->location) }}</td>
-                                        <td>{{ ucwords($eventround->name) }}</td>
-                                    </tr>
-                                @endforeach
+                            @foreach($eventrounds as $eventround)
+                                <tr onmouseover="this.style.backgroundColor='lightgrey'" onmouseout="this.style.backgroundColor='white'">
+                                    <td>{{ ucwords($eventround->roundname) }}</td>
+                                    <td class="hidden-xs">{{ ucwords($eventround->location) }}</td>
+                                    <td>{{ ucwords($eventround->name) }}</td>
+                                </tr>
+                            @endforeach
                             </tbody>
                         </table>
                     </div>
@@ -86,43 +86,43 @@
                         {{ csrf_field() }}
 
                         @foreach($eventrounds as $eventround)
-                            @if ($eventround->usercount < 1)
+                            @if ($eventround->usercount <= 0)
                                 @continue
                             @endif
                             <div class=" table-condensed table-striped table-responsive">
                                 <h4>{!! $eventround->roundname !!}</h4>
-                            <table class="table">
-                                <thead>
-                                <tr>
-                                    <th class="hidden">UserID</th>
-                                    <th class="col-md-1 col-xs-1 col-sm-1">Archer</th>
-                                    <th class="col-md-1 col-xs-1 col-sm-1">Division</th>
+                                <table class="table">
+                                    <thead>
+                                    <tr>
+                                        <th class="hidden">UserID</th>
+                                        <th class="col-md-1 col-xs-1 col-sm-1">Archer</th>
+                                        <th class="col-md-1 col-xs-1 col-sm-1">Division</th>
 
-                                    @if (!is_null($eventround->dist1))
-                                        <th class="col-md-1 col-xs-1 col-sm-1" style="text-align: center;">{!! $eventround->dist1 . $eventround->unit !!}*
-                                        </th>
-                                    @endif
-                                    @if (!is_null($eventround->dist2))
-                                        <th class="col-md-1 col-xs-1 col-sm-1" style="text-align: center;">{!! $eventround->dist2 . $eventround->unit !!}
-                                        </th>
-                                    @endif
-                                    @if (!is_null($eventround->dist3))
-                                        <th class="col-md-1 col-xs-1 col-sm-1" style="text-align: center;">{!! $eventround->dist3 . $eventround->unit !!}
-                                        </th>
-                                    @endif
-                                    @if (!is_null($eventround->dist4))
-                                        <th class="col-md-1 col-xs-1 col-sm-1" style="text-align: center;">{!! $eventround->dist4 . $eventround->unit !!}
-                                        </th>
-                                    @endif
-                                    <th class="col-md-1 col-xs-1 col-sm-1" style="text-align: center;">Total*</th>
-                                    <th class="col-md-1 col-xs-1 col-sm-1" style="text-align: center;">Hits</th>
-                                    <th class="col-md-1 col-xs-1 col-sm-1" style="text-align: center;">10+X</th>
-                                    <th class="col-md-1 col-xs-1 col-sm-1" style="text-align: center;">X</th>
-                                </tr>
-                                </thead>
-                                <tbody>
+                                        @if (!is_null($eventround->dist1))
+                                            <th class="col-md-1 col-xs-1 col-sm-1" style="text-align: center;">{!! $eventround->dist1 . $eventround->unit !!}*
+                                            </th>
+                                        @endif
+                                        @if (!is_null($eventround->dist2))
+                                            <th class="col-md-1 col-xs-1 col-sm-1" style="text-align: center;">{!! $eventround->dist2 . $eventround->unit !!}
+                                            </th>
+                                        @endif
+                                        @if (!is_null($eventround->dist3))
+                                            <th class="col-md-1 col-xs-1 col-sm-1" style="text-align: center;">{!! $eventround->dist3 . $eventround->unit !!}
+                                            </th>
+                                        @endif
+                                        @if (!is_null($eventround->dist4))
+                                            <th class="col-md-1 col-xs-1 col-sm-1" style="text-align: center;">{!! $eventround->dist4 . $eventround->unit !!}
+                                            </th>
+                                        @endif
+                                        <th class="col-md-1 col-xs-1 col-sm-1" style="text-align: center;">Total*</th>
+                                        <th class="col-md-1 col-xs-1 col-sm-1" style="text-align: center;">Hits</th>
+                                        <th class="col-md-1 col-xs-1 col-sm-1" style="text-align: center;">10+X</th>
+                                        <th class="col-md-1 col-xs-1 col-sm-1" style="text-align: center;">X</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
                                     @foreach($users as $user)
-                                        @if(!in_array($user->divisionid, $eventround->divisions))
+                                        @if($user->eventroundid !== $eventround->eventroundid)
                                             @continue
                                         @endif
                                         <tr>
@@ -134,7 +134,7 @@
                                             <td>{{$user->fullname}}</td>
                                             <td>{{$user->divisionname}}</td>
 
-                                            @if(isset($distances['Distance-1']))
+                                            @if (!empty($eventround->dist1))
                                                 <td>
                                                     <input type="text" class="form-control distance" name="distance1[{{$user->evententryid}}][total]" data-formtype="1" data-userrow1="{!! md5($user->evententryid . $user->divisionname) !!}" value="{{ old("distance1.$user->evententryid.total") ?? $user->result->distance1_total ?? '' }}">
                                                     <span class="label label-warning addmoredetails" data-row="{{$user->evententryid}}-1">Add more details</span>
@@ -143,7 +143,7 @@
                                                     <input type="text" placeholder="Xs" class="form-control hidden" data-id="{{$user->evententryid}}-1" name="distance1[{{$user->evententryid}}][x]" value="{{ old("distance1.$user->evententryid.x") ?? $user->result->distance1_x ?? '' }}">
                                                 </td>
                                             @endif
-                                            @if(isset($distances['Distance-2']))
+                                            @if (!empty($eventround->dist2))
                                                 <td>
                                                     <input type="text" class="form-control distance" name="distance2[{{$user->evententryid}}][total]" data-formtype="1" data-userrow1="{!! md5($user->evententryid . $user->divisionname) !!}" value="{{ old("distance2.$user->evententryid.total") ?? $user->result->distance2_total ?? '' }}">
                                                     <span class="label label-warning addmoredetails" data-row="{{$user->evententryid}}-2">Add more details</span>
@@ -152,7 +152,7 @@
                                                     <input type="text" placeholder="Xs" class="form-control hidden" data-id="{{$user->evententryid}}-2" name="distance2[{{$user->evententryid}}][x]" value="{{ old("distance2.$user->evententryid.x") ?? $user->result->distance2_x ?? '' }}">
                                                 </td>
                                             @endif
-                                            @if(isset($distances['Distance-3']))
+                                            @if (!empty($eventround->dist3))
                                                 <td>
                                                     <input type="text" class="form-control distance" name="distance3[{{$user->evententryid}}][total]" data-formtype="1" data-userrow1="{!! md5($user->evententryid . $user->divisionname) !!}" value="{{ old("distance3.$user->evententryid.total") ?? $user->result->distance3_total ?? '' }}">
                                                     <span class="label label-warning addmoredetails" data-row="{{$user->evententryid}}-3">Add more details</span>
@@ -161,7 +161,7 @@
                                                     <input type="text" placeholder="Xs" class="form-control hidden" data-id="{{$user->evententryid}}-3" name="distance3[{{$user->evententryid}}][x]" value="{{ old("distance3.$user->evententryid.x") ?? $user->result->distance3_x ?? '' }}">
                                                 </td>
                                             @endif
-                                            @if(isset($distances['Distance-4']))
+                                            @if (!empty($eventround->dist14))
                                                 <td>
                                                     <input type="text" class="form-control distance" name="distance4[{{$user->evententryid}}][total]" data-formtype="1" data-userrow1="{!! md5($user->evententryid . $user->divisionname) !!}" value="{{ old("distance4.$user->evententryid.total") ?? $user->result->distance4_total ?? '' }}">
                                                     <span class="label label-warning addmoredetails" data-row="{{$user->evententryid}}-4">Add more details</span>
@@ -171,21 +171,25 @@
                                                 </td>
                                             @endif
 
-                                            <td><input type="text" class="form-control" name="total[{{$user->evententryid}}][total]" data-id="total-{!! md5($user->evententryid . $user->divisionname) !!}" data-usertotal="{!! md5($user->evententryid . $user->divisionname) !!}" value="{{ old("total.$user->evententryid.total") ?? $user->result->total_score ?? ''}}"></td>
+
+                                            <td>
+                                                <input type="text" class="form-control" name="total[{{$user->evententryid}}][total]" data-id="total-{!! md5($user->evententryid . $user->divisionname) !!}" data-usertotal="{!! md5($user->evententryid . $user->divisionname) !!}" value="{{ old("total.$user->evententryid.total") ?? $user->result->total_score ?? ''}}">
+                                            </td>
                                             <td><input type="text" class="form-control" name="hits[{{$user->evententryid}}][hits]" placeholder="Optional" value="{{ old("hits.$user->evententryid.hits") ?? $user->result->total_hits ?? ''}}"></td>
                                             <td><input type="text" class="form-control" name="10[{{$user->evententryid}}][10]" placeholder="Optional" value="{{ old("10.$user->evententryid.10") ?? $user->result->total_10 ?? ''}}"></td>
                                             <td><input type="text" class="form-control" name="x[{{$user->evententryid}}][x]" placeholder="Optional" value="{{ old("x.$user->evententryid.x") ?? $user->result->total_x ?? ''}}"></td>
                                         </tr>
                                     @endforeach
-                                </tbody>
-                            </table>
-                        </div>
+                                    </tbody>
+                                </table>
+                            </div>
                         @endforeach
                         {{--end foreach here--}}
-
+                        @if(!empty($eventrounds))
                         <button type="submit" class="btn btn-success pull-right" value="submit" name="submit">
                             Submit Scores
                         </button>
+                        @endif
 
                     </form>
                 </div>
@@ -205,7 +209,7 @@
             @foreach ($users as $user)
                 <div class="box">
                     <div class="box-body">
-                        <form class="form-horizontal" method="POST" action="{{ route('enterscores', [$eventrounds[0]->eventroundid, $event->eventid,  $event->currentweek])  }}" id="scoringformmobile">
+                        <form class="form-horizontal" method="POST" action="{{ route('entereventscores', [$event->eventid])   }}" id="scoringformmobile">
                             {{ csrf_field() }}
 
                             <div class=" table-condensed table-striped table-responsive">
@@ -314,4 +318,4 @@
         </div>{{-- .box--}}
     </div>
 
-    @endsection
+@endsection
