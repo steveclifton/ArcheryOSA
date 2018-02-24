ef9f697db3404e6a6a1b2671bded9731d7e5ac8b
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-02-24 21:42:44 +1300

    Add methods

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,9 +6,22 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Symfony\Component\Process\Process;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+    public function touchurl($url)
+    {
+        $curl = 'curl --location -k --max-time 60 --speed-time 10 --speed-limit 999999999999999 --silent ' . getenv('BASE_URL') . '/' . $url . ' > /dev/null &';
+        shell_exec($curl);
+    }
+
+
+    public function getEventUrl($eventname)
+    {
+        return 'https://archeryosa.com/eventdetails/' . urlencode($eventname);
+
+    }
 }
