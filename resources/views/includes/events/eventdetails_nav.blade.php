3c5b5dd7feac98041b0052f94eec058e740abea2
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-03-08 20:27:30 +1300

    Update nav

--- resources/views/includes/events/eventdetails_nav.blade.php
+++ resources/views/includes/events/eventdetails_nav.blade.php
@@ -3,15 +3,16 @@
         <div class="col-xs-12 col-md-12 pull-right" >
             <div class="box">
                 <div class="box-body">
-                    @if ($event->visible || !empty($userevententry))
+                    @if (($event->visible || !empty($userevententry)) && $event->status != 'completed')
                         @if (!empty($userevententry))
                             <a href="{{ route('updateeventregistrationview', $event->eventid) }}" class="btn btn-warning pull-right eventButtons" role="button">My Entry</a>
-                        @else
+                        @elseif($event->status == 'open')
                             <a href="{{ route('eventregistrationview', ['eventid' => $event->eventid, 'name' => urlencode($event->name)] ) }}" class="btn btn-primary pull-right eventButtons" role="button">
                                 <i class="fa fa-mail-forward" aria-hidden="true"></i> Enter
                             </a>
                         @endif
                     @endif
+
                     @if ($event->eventid == 36 && !empty(Auth::user()->usertype ?? -1 == 1))
                         <a class="btn btn-primary eventButtons pull-right" href="{{route('geteventresults', urlencode($event->name)) }}" role="button">Results</a>
                     @endif
