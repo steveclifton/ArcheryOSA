282c817f75ce70284b8465b7e07d2aea457baff3
Steve <stephen.clifton.1@uni.massey.ac.nz>
2018-03-24 13:33:42 +1300

    Update

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,14 +9,14 @@ class TestController extends Controller
 {
     public function test()
     {
-        $users = User::get();
-        foreach ($users as $user) {
-            $user->username = $user->firstname . $user->lastname;
-            $user->username = strtolower(preg_replace("/[^a-zA-Z0-9]/", "", $user->username)) . rand(1,1440);
-
-            dump($user->username);
-            $user->save();
-        }
-        dd($users);
+//        $users = User::get();
+//        foreach ($users as $user) {
+//            $user->username = $user->firstname . $user->lastname;
+//            $user->username = strtolower(preg_replace("/[^a-zA-Z0-9]/", "", $user->username)) . rand(1,1440);
+//
+//            dump($user->username);
+//            $user->save();
+//        }
+        dd(phpinfo());
     }
 }
